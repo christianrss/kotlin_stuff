14ad9e33ee8b2115a4033ff1c838f2799b2eb9d6
Christian <christianparadix@gmail.com>
2024-06-13 12:26:08 -0300

    Binary Search

---
+++ binary_search.kt
@@ -0,0 +1,29 @@
+package binarysearch.algorithms
+
+import kotlin.text.*
+
+fun main() {
+    val input = readLine()!!.trim().split(" ").map{ it -> it.toInt() }.toIntArray()
+    val eleToSearch = readLine()!!.trim().toInt()
+    val pos = binarySearchInterative(input, eleToSearch)
+    if (pos >= 0) {
+        println(pos)
+    } else {
+        println("Position not found")
+    }
+}
+
+fun binarySearchInterative(input: IntArray, eleToSearch: Int) : Int {
+    var low = 0
+    var high = input.size -1
+    var mid: Int
+    while(low <= high) {
+        mid = low + ((high - low) / 2)
+        when {
+            eleToSearch > input[mid] -> low = mid + 1
+            eleToSearch == input[mid] -> return mid
+            eleToSearch < input[mid] -> high = mid - 1
+        }
+    }
+    return -1
+}
---
+++ compile.bat
@@ -0,0 +1,2 @@
+kotlinc binary_search.kt -include-runtime -d binary_search.jar
+java -jar binary_search.jar
